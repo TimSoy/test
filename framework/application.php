49c4be1e7a86edbbbffef92e28850d06cb8156ae
TimSoy <skd88@mail.ru>
2015-09-19 20:07:45 +0300

    hello2

--- framework/application.php
+++ framework/application.php
@@ -15,15 +15,5 @@ class Application
 	}
 }
 
-/* Router - находит соответствие введенного URI с имеющиемися файлами 
-
-if ($route= Router -> find($ur))
-{
-	$route;
-
-	$ctrl = $route['controller'];
-	$refl = new ReflectionClass($ctrl);
-	$refl -> has Method($route['action']);
-		invoke();
-}
+/* 
 */
